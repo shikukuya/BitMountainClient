510c833230165cf913e8ea4f3220f9dc830d5593
shikukuya <shikukuya@qq.com>
2023-05-07 10:21:07 +0800

    添加本地调试模式

--- src/globalData/socketObject.js
+++ src/globalData/socketObject.js
@@ -1,26 +1,26 @@
-import {io} from "socket.io-client";
+import { io } from 'socket.io-client';
 
 // import serverAddress from "../utils/js/serverAddress";
 
-console.log("SOCKET_OBJ 定义前");
+console.log('SOCKET_OBJ 定义前');
 // let SOCKET_OBJ = io.connect(`${serverAddress}/bitMountain`, {transports: ['websocket']});
 let socketUrl = `ws://127.0.0.1:65533`;
-// let socketUrl = `ws://124.221.150.160:65533`;
+let socketUrlDebug = `ws://124.221.150.160:65533`;
 
 // let SOCKET_OBJ = io.connect(socketUrl, {transports: ['websocket']});
 let SOCKET_OBJ = io.connect(socketUrl, {
-  transports: ["websocket"]
+  transports: ['websocket'],
 });
-console.log("SOCKET_OBJ 定义结束");
+console.log('SOCKET_OBJ 定义结束');
 
-SOCKET_OBJ.on("connect", () => {
-  console.log("SOCKET_OBJ 连接服务器成功");
-})
+SOCKET_OBJ.on('connect', () => {
+  console.log('SOCKET_OBJ 连接服务器成功');
+});
 
 // socket.io 固有事件：丢失连接时触发时间处理器
 SOCKET_OBJ.on('disconnect', (timeout) => {
-  console.log('链接丢失')
-  SOCKET_OBJ.close();  // 关闭连接
+  console.log('链接丢失');
+  SOCKET_OBJ.close(); // 关闭连接
 
   // myAlert("服务器断开连接，1秒后重连");
   // setTimeout(() => {
@@ -31,6 +31,8 @@ SOCKET_OBJ.on('disconnect', (timeout) => {
 
 SOCKET_OBJ.on('connect_error', (err) => {
   console.warn('SOCKET_OBJ 连接失败：', err);
+  console.log('SOCKET_OBJ 启用本地调试模式');
+  SOCKET_OBJ.io.uri = socketUrlDebug;
 });
 
 SOCKET_OBJ.on('connect_timeout', () => {
@@ -52,6 +54,5 @@ SOCKET_OBJ.on('reconnect_attempt', (attemptNumber) => {
 SOCKET_OBJ.on('reconnect_failed', () => {
   console.warn('SOCKET_OBJ 重新连接失败');
 });
-
 
 export default SOCKET_OBJ;
